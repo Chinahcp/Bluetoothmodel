c9c1e00f1d24bdc878286d157928ebe5596eba9c
hcp <houcp@zucc.edu.cn>
2022-01-13 11:08:44 +0800

    首次提交

---
+++ BluetoothTransmission/src/main/java/com/zucc/hcp/bluetoothtransmission/common/Constants.java
@@ -0,0 +1,14 @@
+package com.zucc.hcp.bluetoothtransmission.common;
+
+public class Constants {
+
+    /*intent tag*/
+    public static final String INTENT_IP="intentIp";//ip
+    public static final String INTENT_PORT="intentPort";//port(端口号)
+
+
+    /*EventBus  msg*/
+    public static final String CONNET_SUCCESS="connectSucccess";
+
+    public static final String UPDATA="updata";
+}
---
+++ BluetoothTransmission/src/main/java/com/zucc/hcp/bluetoothtransmission/common/EventMsg.java
@@ -0,0 +1,15 @@
+package com.zucc.hcp.bluetoothtransmission.common;
+
+public class EventMsg {
+
+    private String Tag;
+
+
+    public String getTag() {
+        return Tag;
+    }
+
+    public void setTag(String tag) {
+        Tag = tag;
+    }
+}
